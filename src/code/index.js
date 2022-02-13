a2728736961c8cb3fa1b91e4378f7abebd3509c9
Candela Bramucci <candelabramucci05@gmail.com>
2022-02-12 21:35:38 -0300

    carousel styles and code

--- src/code/index.js
+++ src/code/index.js
@@ -1,3 +1,4 @@
+// navigation
 const primaryNav = document.getElementById('primary-nav')
 const navToggle = document.querySelector('.mobile-nav-toggle')
 const hamburger = navToggle.querySelector('.hamburger')
@@ -16,3 +17,40 @@ navToggle.addEventListener('click', () => {
 
     hamburger.classList.toggle('open')
 })
+
+// carousel
+
+const hero = document.querySelector('.hero')
+
+const headerText = document.querySelector('header section')
+const title = headerText.querySelector('h1')
+const p = headerText.querySelector('p')
+const btns = [...document.querySelectorAll('[data-carousel-btn]')]
+
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const offset = btn.dataset.carouselBtn === 'next' ? 1 : -1
+        const actualIndex = Number(hero.dataset.carouselIndex)
+        const index = changeIndex(actualIndex, offset)
+
+        hero.dataset.carouselIndex = index
+        changeHeaderText(index)
+    })
+})
+
+function changeIndex(index, value) {
+    index += value
+
+    if (index < 1) index = 3
+    if (index > 3) index = 1
+
+    return index
+}
+
+async function changeHeaderText(i) {
+    const data = await fetch('src/code/carousel.json')
+    const obj = await data.json()
+
+    title.innerText = obj[i].title
+    p.innerText = obj[i].p
+}
